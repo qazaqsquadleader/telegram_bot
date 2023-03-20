7298ad80d0cfa34b9124d7fc078ec30306668b1b
aromanov <nanaaliroma228@gmail.com>
2023-03-20 16:44:57 +0600

    first lesson

---
+++ cmd/web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	t := mustToken()
+}
+
+func mustToken() string {
+	token := flag.String("token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+	flag.Parse()
+	if *token == "" {
+		log.Fatal("Token length equal 0")
+	}
+	return *token
+}
